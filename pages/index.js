85ade5843f4058a0da2d17f351677c90fbcf15ec
Rokas <rookas.rudzenskas@gmail.com>
2021-07-31 11:57:57 +0300

    Rokas

--- pages/index.js
+++ pages/index.js
@@ -12,11 +12,11 @@ export default function Home() {
 
         <div className="">
             <div className="h-16 bg-gray-900 flex flex-row items-center">
-                <div className="w-1/5 h-16 ml-10 pt-2">
+                <div className="w-1/5 h-16 ml-10 pt-1">
                     <img className="w-28 h-16 object-contain" src="https://www.pinclipart.com/picdir/big/57-576184_view-our-amazon-storefront-amazon-logo-white-png.png" />
                 </div>
 
-                <input type="text" id="Search" placeholder="Search" className="h-9 w-30 bg-gray-800"/>
+                <input type="text" id="Search" placeholder="Search" className="h-9 w-30 bg-gray-800 border border-gray-500 border-opacity-75 rounded-l-xl outline-none px-3"/>
 
             </div>
             {/*<Header />*/}
