2bc1bfbfc4947d282495943426c97c89be023dfd
Rokas <rookas.rudzenskas@gmail.com>
2021-07-31 11:13:27 +0300

    Rokas

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Header = () => {
     return (
         <div className="main">
-
+            <div className="logo">
+                <img src="/amazon-logo.png" />
+            </div>
         </div>
     );
 };
