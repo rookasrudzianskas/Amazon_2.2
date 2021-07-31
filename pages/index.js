15bacb851215bb0ef7323f4fe277356ec97f5b25
Rokas <rookas.rudzenskas@gmail.com>
2021-07-31 11:22:40 +0300

    Rokas

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Header from "../src/components/Header";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
       <Head>
         <title>Amazon 2.2 App</title>
         <link rel="icon" href="/amazon-2.png" />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Image from 'next/image';
 
 const Header = () => {
     return (
         <div className="main">
-            <div className="logo">
-                <img src="/amazon-logo.png" />
+            <div className="bg-red-200">
+                <img className="w-1/5 h-16" src="https://www.pinclipart.com/picdir/big/57-576184_view-our-amazon-storefront-amazon-logo-white-png.png" />
             </div>
         </div>
     );
