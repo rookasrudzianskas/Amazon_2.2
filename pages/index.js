b78c705c05e2de417892ac5fdd4c58a4d27537c6
Rokas <rookas.rudzenskas@gmail.com>
2021-07-31 12:09:11 +0300

    Rokas

--- pages/index.js
+++ pages/index.js
@@ -18,11 +18,18 @@ export default function Home() {
                     <img className="w-28 h-16 object-contain" src="https://www.pinclipart.com/picdir/big/57-576184_view-our-amazon-storefront-amazon-logo-white-png.png" />
                 </div>
 
-                <input type="text" id="Search" placeholder="Search" className="h-9 w-30 bg-gray-800 border border-gray-500 border-opacity-75 rounded-l-xl outline-none px-3 text-white ml-6"/>
+                <input type="text" id="Search" placeholder="Search" className="placeholder-white h-9 w-30 bg-gray-800 border border-gray-500 border-opacity-75 rounded-l-xl outline-none px-3 text-white ml-6"/>
 
-                <div className="h-9 w-30 border border-gray-500 border-opacity-75 px-3 bg-gray-800">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <div className="h-9 w-30 border border-gray-500 border-opacity-75 px-3 bg-gray-800 items-center flex">
+                        <div className="text-white mr-3">categories</div>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="white">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+                    </svg>
+                </div>
+
+                <div className="bg-yellow-500">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="white">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                     </svg>
                 </div>
 
