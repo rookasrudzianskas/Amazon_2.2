2dd729bf3850422e4ff2a6b54d4b8d1972c809a6
Rokas <rookas.rudzenskas@gmail.com>
2021-07-31 11:49:05 +0300

    Rokas

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import React from "react";
-import Header from "../src/components/Header";
+// import Header from "../src/components/Header";
 
 export default function Home() {
   return (
@@ -10,8 +10,13 @@ export default function Home() {
         <link rel="icon" href="/amazon-2.png" />
       </Head>
 
-        <div>
-            <Header />
+        <div className="">
+            <div className="h-16 bg-gray-900	">
+                <div className="w-1/5 h-16">
+                    <img className="w-28 h-16 object-contain" src="https://www.pinclipart.com/picdir/big/57-576184_view-our-amazon-storefront-amazon-logo-white-png.png" />
+                </div>
+            </div>
+            {/*<Header />*/}
         </div>
                 {/*<Header />*/}
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,7 @@ import Image from 'next/image';
 
 const Header = () => {
     return (
-        <div className="h-16 bg-gray-900	">
-            <div className="w-1/5 h-16">
-                <img className="" src="https://www.pinclipart.com/picdir/big/57-576184_view-our-amazon-storefront-amazon-logo-white-png.png" />
-            </div>
-        </div>
+
     );
 };
 
