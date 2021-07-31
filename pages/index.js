3d16545218a4f3849a071515d97d2fc305644c15
Rokas <rookas.rudzenskas@gmail.com>
2021-07-31 11:37:51 +0300

    Rokas

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,16 @@
 import Head from 'next/head'
 import Header from "../src/components/Header";
+import React from "react";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="bg-white w-full">
       <Head>
         <title>Amazon 2.2 App</title>
         <link rel="icon" href="/amazon-2.png" />
       </Head>
 
-          <div className="bg-white w-full">
+          <div className="">
                 <Header />
 
           </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,9 @@ import Image from 'next/image';
 
 const Header = () => {
     return (
-        <div className="main">
-            <div className="bg-red-200">
-                <img className="w-1/5 h-16" src="https://www.pinclipart.com/picdir/big/57-576184_view-our-amazon-storefront-amazon-logo-white-png.png" />
+        <div className="bg-gray-900	">
+            <div className="w-1/5 h-36">
+                <img className="" src="https://www.pinclipart.com/picdir/big/57-576184_view-our-amazon-storefront-amazon-logo-white-png.png" />
             </div>
         </div>
     );
