c66cea50e824b8dc2c677f4bd1df11ddd8b85ef8
Rokas <rookas.rudzenskas@gmail.com>
2021-07-31 12:18:33 +0300

    Rokas

--- pages/index.js
+++ pages/index.js
@@ -34,8 +34,18 @@ export default function Home() {
                 </div>
 
 
-                <div className="">
+                <div className="text-white flex">
+                    <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                    </svg>
+                    </div>
 
+                    <div className="h-10 w-10 bg-yellow-500 flex items-center justify-center rounded-xl text-sm">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                        </svg>
+                    </div>
                 </div>
 
 
