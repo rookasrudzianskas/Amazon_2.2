24620d2f45f24386f603757ac5d59ed32bf6c96c
Rokas <rookas.rudzenskas@gmail.com>
2021-07-31 12:24:51 +0300

    Rokas

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,7 @@ export default function Home() {
                 </div>
 
 
-                <div className="text-white flex items-center ml-auto mr-8 w-48 h-16">
+                <div className="text-white flex items-center ml-auto mr-8 w-48 h-16 justify-around">
                     <div>
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
