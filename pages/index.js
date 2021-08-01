69fa52f0ddf63f414f476e482148a0386ec2d632
Rokas <rookas.rudzenskas@gmail.com>
2021-08-01 18:17:45 +0300

    Rokas

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,6 @@ export default function Home() {
     // const png = jdenticon.toPng(value, size);
     // const png = jdenticon.toPng(value, size);
     // const png = jdenticon.toPng(value, size);
-    // const png = jdenticon.toPng(value, size);
 
 
     return (
