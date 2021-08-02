966b0288880d812d287658f5dc44459dbc6e9219
Rokas <rookas.rudzenskas@gmail.com>
2021-08-02 21:12:55 +0300

    Rokas

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,9 +5,9 @@ const Sidebar = () => {
         <div>
             <div className="w-1/5 bg-gray-900">
                 <div>
-                    <div className="text-yellow-500 cursor-pointer flex">
-                        <span className="">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="#F59E0C">
+                    <div className="text-yellow-500 cursor-pointer flex items-center ">
+                        <span className="w-8">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="#F59E0C">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
                             </svg>
                         </span>
