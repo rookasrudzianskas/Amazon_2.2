97ec10d5ca19fe3878ae7ef1f2e8fd1018c7b3b0
Rokas <rookas.rudzenskas@gmail.com>
2021-08-03 09:57:59 +0300

    Rokas

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
                 <div className="flex-1 p-6">
                     {/*<Section />*/}
                     <div className="h-44 w-full main-section-banner rounded-lg">
-                        <div className="button bg-white w-36 h-10 rounded-full">
+                        <div className="button bg-white w-36 h-10 rounded-full flex justify-center items-center ml-4">
                             Browse Products
                         </div>
                     </div>
