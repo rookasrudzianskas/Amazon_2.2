ed78d97f341eee38dcda7c4e92e92099c6dd89a1
Rokas <rookas.rudzenskas@gmail.com>
2021-08-03 08:26:29 +0300

    Rokas

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,23 @@ export default function Home() {
             <div className="h-screen flex">
 
                 <div className="w-1/5 bg-gray-900 border-gray-700 border-t-2">
-                    <Sidebar />
+                    <div className="w-1/5 bg-gray-900 p-6">
+                        <div>
+                            <div className="text-yellow-500 cursor-pointer flex items-center font-bold ">
+                        <span className="w-8 mr-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="#F59E0C">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
+                            </svg>
+                        </span>
+
+                                <span className="">Categories</span>
+                            </div>
+
+                            <div className="">
+
+                            </div>
+                        </div>
+                    </div>
 
                 </div>
 
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,21 +3,7 @@ import React from 'react';
 const Sidebar = () => {
     return (
         <div>
-            <div className="w-1/5 bg-gray-900">
-                <div>
-                    <div className="text-yellow-500 cursor-pointer flex items-center ">
-                        <span className="w-8">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="#F59E0C">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
-                            </svg>
-                        </span>
 
-                        <span className="">Categories</span>
-
-
-                    </div>
-                </div>
-            </div>
         </div>
     );
 };
