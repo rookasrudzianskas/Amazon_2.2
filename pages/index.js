2384fcfb253f880f69f1e906014e81dec427e748
Rokas <rookas.rudzenskas@gmail.com>
2021-08-03 10:34:13 +0300

    Rokas

--- pages/index.js
+++ pages/index.js
@@ -123,8 +123,14 @@ export default function Home() {
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
                                 </svg>
                             </div>
+                        </div>
+                    </div>
 
+                    <div className="main-section-deals">
+                        <h1 className="text-2xl font-bold text-gray-700 mb-5 mt-10">Hot Deals 🚀</h1>
 
+                        <div className="">
+                            
                         </div>
                     </div>
 
