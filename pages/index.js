0d44ff1469a5482e647c70157ed6a50347cb7cd1
Rokas <rookas.rudzenskas@gmail.com>
2021-08-03 09:18:21 +0300

    Rokas

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home() {
 
                 <div className="w-1/5 bg-gray-900 border-gray-700 border-t-2  p-6 overflow-hidden">
                         <div className="">
-                            <div className="text-yellow-500 cursor-pointer flex items-center font-bold mb-4 p-2">
+                            <div className=" bg-gray-700 rounded-lg text-yellow-500 cursor-pointer flex items-center font-bold mb-4 p-2">
                         <span className="w-8 mr-1">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="#F59E0C">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
