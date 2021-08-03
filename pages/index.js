6b6583e85325386f63b2e6800ba0451e264a614a
Rokas <rookas.rudzenskas@gmail.com>
2021-08-03 09:47:32 +0300

    Rokas

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,10 @@ export default function Home() {
                     <Sidebar />
                 </div>
                 <div>
-                    <Section />
+                    {/*<Section />*/}
+                    <div className="h-44">
+                        <img src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_45M_v2_1x._CB432458380_.jpg" alt=""/>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/Section.js
+++ src/components/Section.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Section = () => {
     return (
         <div>
-            <img src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_45M_v2_1x._CB432458380_.jpg" alt=""/>
+
         </div>
     );
 };
