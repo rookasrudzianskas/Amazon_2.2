89d18f887dd8a41e39c2caaa9a7a525953ee205b
Rokas <rookas.rudzenskas@gmail.com>
2021-08-04 14:35:42 +0300

    Rokas

--- pages/cart-section.js
+++ pages/cart-section.js
@@ -4,6 +4,12 @@ const CartSection = () => {
     return (
         <div>
             <h1 className="text-gray-400 text-3xl mb-6 font-bold">Shopping Cart</h1>
+
+            <div className="flex flex-row font-bold text-gray-400">
+                <h2 className="flex flex-grow">Product</h2>
+                <h2 className="w-48">Count</h2>
+                <h2 className="w-48">Total Cost</h2>
+            </div>
         </div>
     );
 };
