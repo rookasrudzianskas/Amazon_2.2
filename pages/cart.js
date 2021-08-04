81cc6269497e7179e30fc5d02c9ebf985af7ac66
Rokas <rookas.rudzenskas@gmail.com>
2021-08-04 13:41:14 +0300

    Rokas

--- pages/cart.js
+++ pages/cart.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Head from "next/head";
 
 const Cart = () => {
     return (
-        <div>
-           Cart
+        <div className="bg-white w-full h-screen overflow-hidden">
+            <Head>
+                <title>Amazon Cart</title>
+                <link rel="icon" href="/amazon-2.png"/>
+            </Head>
+
         </div>
     );
 };
