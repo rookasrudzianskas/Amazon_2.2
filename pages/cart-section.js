b03c488ace11cb72f8925b83014160a344a4cc5d
Rokas <rookas.rudzenskas@gmail.com>
2021-08-04 14:28:45 +0300

    Rokas

--- pages/cart-section.js
+++ pages/cart-section.js
@@ -3,7 +3,7 @@ import React from 'react';
 const CartSection = () => {
     return (
         <div>
-            <h1>Shopping Cart</h1>
+            <h1 className="text-gray-400 text-3xl mb-6 font-bold">Shopping Cart</h1>
         </div>
     );
 };
