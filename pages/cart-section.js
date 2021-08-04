4acccd99a94da2fbd8d034ae81b31d9463cb216f
Rokas <rookas.rudzenskas@gmail.com>
2021-08-04 14:53:20 +0300

    Rokas

--- pages/cart-section.js
+++ pages/cart-section.js
@@ -11,13 +11,19 @@ const CartSection = () => {
                 <h2 className="w-48">Total Cost</h2>
             </div>
 
-            <div className="">
-                <div className="">
+            <div className="mt-5">
+                <div className="flex flex-row items-center">
                     <div className="w-40 h-32 bg-white p-4 flex items-center justify-center rounded-lg">
                         <img className="w-full h-full object-contain"  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwimt_bQG_EXqDOLO8hnBCrj_Ma5_Bju_cHg&usqp=CAU" alt=""/>
                     </div>
                 <div className="">
+                    <div className="">
+                        Apple Macbook 15.5 inch
+                    </div>
 
+                    <div>
+                        Apple
+                    </div>
                 </div>
 
                 <div className="">
