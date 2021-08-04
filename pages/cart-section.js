9d8f5298523f7b15ae235f0f62f0a956d58e78ad
Rokas <rookas.rudzenskas@gmail.com>
2021-08-04 14:49:06 +0300

    Rokas

--- pages/cart-section.js
+++ pages/cart-section.js
@@ -12,7 +12,10 @@ const CartSection = () => {
             </div>
 
             <div className="">
-
+                <div className="">
+                    <div className="w-40 h-32 bg-white p-4 flex items-center justify-center rounded-lg">
+                        <img className="w-full h-full object-contain"  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwimt_bQG_EXqDOLO8hnBCrj_Ma5_Bju_cHg&usqp=CAU" alt=""/>
+                    </div>
                 <div className="">
 
                 </div>
@@ -21,10 +24,11 @@ const CartSection = () => {
 
                 </div>
 
-                <div className="">
+                <div className="delete">
 
                 </div>
 
+                </div>
             </div>
         </div>
     );
