0e6414e0bd7c7d60e486384cf40a1841daadc66a
Rookas Rudzianskas <rookasrudzianskas@Rookass-MacBook-Pro-2.local>
2021-08-06 17:10:02 +0300

    Rokas

--- firebase.js
+++ firebase.js
@@ -12,3 +12,11 @@ const firebaseConfig = {
 };
 
 // initialized
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export {auth, provider};
+export default db;
