2a0a5d3f4c1312229691e7ad5b4ddb28f36df515
Rookas Rudzianskas <rookasrudzianskas@Rookass-MacBook-Pro-2.local>
2021-08-06 18:30:08 +0300

    Rokas

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Image from 'next/image';
 import {useRouter} from "next/router";
+import db from "../../firebase";
 
 
 const Header = () => {
+    const [items, setItems] = useState([]);
 
-    const router = useRouter()
+    const router = useRouter();
+
+    useEffect(() => {
+        db.collection('items').get().then((snapshot) => {
+            setItems(snapshot.docs.map(doc => ({
+                id: doc.id,
+                image: doc.data().image,
+                name: doc.data().title,
+                make: doc.data().brand,
+                rating: doc.data().rating,
+                price: doc.data().price,
+            })));
+        })
+    }, []);
 
     return (
        <div>
@@ -41,7 +56,7 @@ const Header = () => {
 
                    {/* this is card icon*/}
                    <div className="relative text-gray-600 h-10 w-10 bg-yellow-500 flex items-center justify-center rounded-xl cursor-pointer hover:bg-yellow-600 duration-200" onClick={() =>  router.push('/cart')}>
-                       <div className="absolute -top-1 -right-1 bg-white w-4 h-4 rounded-full flex items-center justify-center border-1 border-black solid text-sm">1</div>
+                       <div className="absolute -top-1 -right-1 bg-white w-4 h-4 rounded-full flex items-center justify-center border-1 border-black solid text-sm">{items.length}</div>
                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
                        </svg>
