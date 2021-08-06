c1a0fad6ee908b71bafe1b5d0eb20c9b25af7cb8
Rookas Rudzianskas <rookasrudzianskas@Rookass-MacBook-Pro-2.local>
2021-08-06 19:34:53 +0300

    Rokas

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import 'tailwindcss/tailwind.css';
 import '../styles/globals.css';
+import {Provider} from "react-redux";
+import {store} from "../app/store";
 
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  <Provider store={store}>
+    return <Component {...pageProps} />
+  </Provider>
+
 }
 
 export default MyApp
